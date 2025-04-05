1ca648f05baa3c6cf408c0d60e04cefabbf409e4
kiara-singh <singhkiara317@gmail.com>
2025-04-05 14:46:33 -0700

    updated

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import React, {useState, useEffect} from 'react';
 import Login from './pages/Login';
 import Signup from './pages/Signup';
+import Dashboard from './pages/dashboard';
 import { BrowserRouter as Router} from 'react-router-dom';
 import {Routes, Route} from 'react-router-dom';
 
@@ -13,13 +14,12 @@ function App() {
             <Routes>  
                <Route path="/" element={<Signup/>}/>
                <Route path="/login" element={<Login/>}/>
+               <Route path="/dashboard" element={<Dashboard/>}/>
             </Routes>                    
         </section>
       </div>
     </Router>
   );
 }
-
-export default App;
 
 export default App;
--- src/pages/dashboard.jsx
+++ src/pages/dashboard.jsx
@@ -13,6 +13,8 @@ export default function Dashboard() {
 
   return (
     <div className="flex justify-center items-center mx-auto max-w-[80%] min-h-screen">
+
+
       <form onSubmit={handleSubmit} className="flex flex-col w-1/2 gap-2">
         <label className="font-medium">Transaction</label>
         <input
@@ -29,12 +31,12 @@ export default function Dashboard() {
           placeholder="Cost"
           required
         />
-        <div class="relative inline-block text-left">
+        <div className="relative inline-block text-left">
           <div>
             <button
               type="button"
               onClick={() => setDropDown(!dropDown)}
-              class="inline-flex w-full justify-center gap-x-1.5 rounded-md bg-white px-3 py-2 text-sm font-semibold text-gray-900 ring-1 shadow-xs ring-gray-300 ring-inset hover:bg-gray-50"
+              className="inline-flex w-full justify-center gap-x-1.5 rounded-md bg-white px-3 py-2 text-sm font-semibold text-gray-900 ring-1 shadow-xs ring-gray-300 ring-inset hover:bg-gray-50"
               aria-expanded="true"
               aria-haspopup="true"
             >
@@ -43,17 +45,18 @@ export default function Dashboard() {
           </div>
 
           <div
-            class="absolute right-0 z-10 mt-2 w-56 origin-top-right rounded-md bg-white ring-1 shadow-lg ring-black/5 focus:outline-hidden"
+            className="absolute right-0 z-10 mt-2 w-56 origin-top-right rounded-md bg-white ring-1 shadow-lg ring-black/5 focus:outline-hidden"
             hidden={dropDown}
           >
-            <div class="py-1" role="none">
+            <div className="py-1" role="none">
               {categories.map((categoryName) => (
                 <p
                   onClick={() => {
                     setDropDown(!dropDown);
                     setCategory(categoryName);
                   }}
-                  class="px-4 py-2 text-sm text-gray-700 hover:cursor-pointer"
+                  key={categoryName}
+                  className="px-4 py-2 text-sm text-gray-700 hover:cursor-pointer"
                 >
                   {categoryName}
                 </p>
