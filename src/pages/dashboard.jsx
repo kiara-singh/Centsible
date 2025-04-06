640dacd2480a04901f49543475ab339180a20fea
czhang0003 <ciz003@ucsd.edu>
2025-04-05 17:32:43 -0700

    finish transaction page

--- src/pages/dashboard.jsx
+++ src/pages/dashboard.jsx
@@ -1,12 +1,12 @@
 import React, { useState, useRef } from "react";
-import { collection, addDoc } from "firebase/firestore";
+import { doc, setDoc } from "firebase/firestore";
 import { auth, db } from "../firebase";
-import { categories } from "../constants";
 
 export default function Dashboard() {
   const [dropDown, setDropDown] = useState(false);
   const [error, setError] = useState("");
 
+  const categories = ["Groceries", "Education", "Fun", "Travel"];
   const notes = useRef("");
   const cost = useRef(0);
   const [category, setCategory] = useState("Choose category");
@@ -16,7 +16,7 @@ export default function Dashboard() {
     setError("");
 
     try {
-      await addDoc(collection(db, "transactions"), {
+      await setDoc(doc(db, "transactions", "transaction"), {
         notes: notes.current.value,
         amount: cost.current.value,
         category: category,
@@ -29,12 +29,14 @@ export default function Dashboard() {
   };
 
   return (
+    <div className="flex bg-[#f9faf3]">
     <div className="flex justify-center items-center mx-auto max-w-[80%] min-h-screen">
-      <form onSubmit={handleSubmit} className="flex flex-col w-1/2 gap-2">
-        <label className="font-medium">Transaction</label>
+
+      <h1 class="text-3xl font-bold font-Roboto absolute top-0 w-full py-4 bg-[#f9faf3] text-center text-[#012a57]">Centsible</h1>
+      <form onSubmit={handleSubmit} className="absolute top-50 left-1/2 transform -translate-x-1/2 flex flex-col w-7/8 gap-5">
+        <label className="font-medium text-center text-[#012a57] font-Roboto text-2xl">Add Your Transaction</label>
         <input
           type="number"
-          step="0.01"
           ref={cost}
           className="border border-gray-300 text-black text-sm rounded-md w-full p-2.5 y-600 placeholder:text-gray-400"
           placeholder="Cost"
@@ -80,15 +82,19 @@ export default function Dashboard() {
             </div>
           </div>
         </div>
-
+        <div className="items-center justify-center min-h-screen">
+        <center>
         <button
           type="submit"
-          className="hover:cursor-pointer bg-slate-300 font-bold rounded-xl p-2 w-fit"
+          className="hover:cursor-pointer bg-[#012a57] font-bold rounded-xl p-2 w-fit text-[#f9faf3]"
         >
           Log Transaction
         </button>
+        </center>
+        </div>
         {error && <p className="text-red-900">{error}</p>}
       </form>
+    </div>
     </div>
   );
 }
