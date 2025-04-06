f3df5d18d8977399b3cb869e2bd990ad10790a80
n1sh1thaS <nishi2919@gmail.com>
2025-04-06 10:05:12 -0700

    voice command fixes

--- src/pages/voiceCommand.jsx
+++ src/pages/voiceCommand.jsx
@@ -1,7 +1,7 @@
 import React, { useState, useRef } from "react";
 import { categories } from "../constants";
-
-const SpeechToText = ({onExtract}) => {
+import { AudioOutlined } from "@ant-design/icons";
+const SpeechToText = ({ onExtract }) => {
   const [isListening, setIsListening] = useState(false);
   const [transcript, setTranscript] = useState("");
   const recognitionRef = useRef(null);
@@ -25,11 +25,11 @@ const SpeechToText = ({onExtract}) => {
 
       recognitionRef.current.onresult = (event) => {
         let interimTranscript = "";
-        let match=null;
+        let match = null;
         for (let i = event.resultIndex; i < event.results.length; i++) {
           const result = event.results[i];
           if (result.isFinal) {
-            const finalText=result[0].transcript.trim();
+            const finalText = result[0].transcript.trim();
             setTranscript((prev) => prev + result[0].transcript + " ");
             console.log("Final Transcript:", finalText);
             const categoryPattern = categories.join("|");
@@ -45,21 +45,26 @@ const SpeechToText = ({onExtract}) => {
           }
         }
 
-       
-
         if (match) {
           const cost = parseFloat(match[1]);
-          const category = match[2];
+          let category = match[2];
+          category = category.charAt(0).toUpperCase() + category.slice(1);
           const note = match[3] ? match[3].trim() : null;
 
-          console.log("Matched values - Cost:", cost, "Category:", category, "Note:", note);
+          console.log(
+            "Matched values - Cost:",
+            cost,
+            "Category:",
+            category,
+            "Note:",
+            note
+          );
 
-
-          if(onExtract){
-            onExtract({cost,category,note});
+          if (onExtract) {
+            onExtract({ cost, category, note });
           }
 
-          console.log("inVC:",cost, category, note);
+          console.log("inVC:", cost, category, note);
         }
       };
 
@@ -78,13 +83,13 @@ const SpeechToText = ({onExtract}) => {
   };
 
   return (
-    <div>
-      <button onClick={toggleListening}>
+    <div className="flex flex-col">
+      <AudioOutlined style={{ fontSize: "25px" }} onClick={toggleListening}>
         {isListening ? "Stop Listening" : "Start Listening"}
-      </button>
-      <p>
+      </AudioOutlined>
+      {/* <p>
         <strong>Transcript:</strong> {transcript}
-      </p>
+      </p> */}
     </div>
   );
 };
