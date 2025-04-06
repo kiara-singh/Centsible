b992f0e75f4c3712fcbab1f7af83006962e96e7f
kiara-singh <singhkiara317@gmail.com>
2025-04-06 10:46:46 -0700

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,5 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
-import { UserProvider } from "./context/UserContext.jsx";
 import "./index.css";
 import App from "./App.jsx";
 
